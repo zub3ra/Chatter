9fad1df5a1f163d51b6a4662741ae31ca6e5f7bc
Marcin Warpechowski <warpech@gmail.com>
2015-05-18 15:28:42 +0200

    UI improvements

--- ViewModels/RoomPage.json.cs
+++ ViewModels/RoomPage.json.cs
@@ -7,12 +7,29 @@ namespace Chatter {
             Statements = Db.SQL<Statement>(@"
 					SELECT m FROM Statement m WHERE m.room = ? 
 					ORDER BY tick DESC LIMIT ?", this.Data, 10);
+
+            //Reverse the array
+            for (int i = 0; i < Statements.Count / 2; i++) {
+                var tmp = Statements[i];
+                Statements[i] = Statements[Statements.Count - i - 1];
+                Statements[Statements.Count - i - 1] = tmp;
+            }
         }
 
         void Handle(Input.Send action) {
+            if (Nick == "") {
+                Warning = "Nick cannot be empty";
+                return;
+            }
+            if (Statement == "") {
+                Warning = "Message cannot be empty";
+                return;
+            }
+            Warning = "";
+
             Db.Transact(() => {
                 var m = new Statement() {
-                    user = Nick == "" ? "Anonymous" : Nick,
+                    user = Nick,
                     room = this.Data,
                     msg = Statement,
                     tick = DateTime.Now.Ticks
