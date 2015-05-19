4bec88ce65fd0865c2606c677d95e9dab8728a51
Marcin Warpechowski <warpech@gmail.com>
2015-05-19 14:15:45 +0200

    notify all WebSocket sessions when there is a new Statement

--- ViewModels/RoomPage.json.cs
+++ ViewModels/RoomPage.json.cs
@@ -1,19 +1,23 @@
 using Starcounter;
+using Starcounter.Internal;
 using System;
 
 namespace Chatter {
+
     partial class RoomPage : Page, IBound<Room> {
+
+        private Int64 maxMsgs = 10;
+
         public void RefreshStatements() {
+            var count = Db.SlowSQL<Int64>(@"
+					SELECT COUNT(*) FROM Statement m 
+                    WHERE m.room = ?", this.Data).First;
+
+            var offset = count > maxMsgs ? count - maxMsgs : 0;
+
             Statements = Db.SQL<Statement>(@"
 					SELECT m FROM Statement m WHERE m.room = ? 
-					ORDER BY tick DESC LIMIT ?", this.Data, 10);
-
-            //Reverse the array
-            for (int i = 0; i < Statements.Count / 2; i++) {
-                var tmp = Statements[i];
-                Statements[i] = Statements[Statements.Count - i - 1];
-                Statements[Statements.Count - i - 1] = tmp;
-            }
+					ORDER BY tick ASC FETCH ? OFFSET ?", this.Data, 10, offset);
         }
 
         void Handle(Input.Send action) {
@@ -26,18 +30,36 @@ namespace Chatter {
                 return;
             }
 
+            Statement m = null;
             Db.Transact(() => {
-                var m = new Statement() {
+                m = new Statement() {
                     user = Nick,
                     room = this.Data,
                     msg = Statement,
                     tick = DateTime.Now.Ticks
                 };
             });
-            RefreshStatements();
 
             Warning = "";
             Statement = "";
+
+            Session.ForEach((Session s) => {
+                StarcounterEnvironment.AppName = "Chatter";
+                if (s.Data is MasterPage) {
+                    MasterPage data = (MasterPage)s.Data;
+                    if (data.CurrentPage is RoomPage) {
+                        RoomPage page = (RoomPage)data.CurrentPage;
+                        if (page.Data.Equals(this.Data)) { //is current room?
+                            if (page.Statements.Count >= maxMsgs) {
+                                page.Statements.RemoveAt(0);
+                            }
+                            var item = page.Statements.Add();
+                            item.Data = m;
+                            s.CalculatePatchAndPushOnWebSocket();
+                        }
+                    }
+                }
+            });
         }
     }
 }
