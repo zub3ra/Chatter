4a7e09c8916e84588fc311725e8fe1f2ab8f6ba2
aloscha <oleksiy.yatsko@gmail.com>
2016-04-27 15:11:32 +0200

    Add bem style

---
+++ wwwroot/Chatter/js/LobbyPageScript.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    var script = document._currentScript || document.currentScript;
+    var template = script.previousElementSibling;
+
+    template.onInputKeyup = function (e) {
+        if (e.keyCode == 13) {
+            template.set("model.GoToNewGroup$", template.model.GoToNewGroup$ + 1);
+        }
+    };
+})();
